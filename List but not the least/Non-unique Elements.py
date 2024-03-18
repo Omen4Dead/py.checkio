ab595a2dfa56becc38d2d9c35a25ef20191520e5
Omen4Dead <69041761+Omen4Dead@users.noreply.github.com>
2024-03-18 22:50:36 +0500

    111

---
+++ List but not the least/Non-unique Elements.py
@@ -0,0 +1,32 @@
+from collections.abc import Iterable
+
+
+def checkio(data: list[int]) -> Iterable[int]:
+    # your code here
+    cnt = dict()
+    for i in data:
+        if i not in cnt: cnt[i] = 1
+        else: cnt[i] += 1
+    for key, val in cnt.items():
+        if val == 1:
+            data.remove(key)
+    return data
+
+
+print("Example:")
+print(list(checkio([1, 2, 3, 1, 3])))
+
+# These "asserts" are used for self-checking
+assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3]
+assert list(checkio([1, 2, 3, 4, 5])) == []
+assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5]
+assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9]
+assert list(checkio([2, 2, 3, 2, 2])) == [2, 2, 2, 2]
+assert list(checkio([10, 20, 30, 10])) == [10, 10]
+assert list(checkio([7])) == []
+assert list(checkio([0, 1, 2, 3, 4, 0, 1, 2, 4])) == [0, 1, 2, 4, 0, 1, 2, 4]
+assert list(checkio([99, 98, 99])) == [99, 99]
+assert list(checkio([0, 0, 0, 1, 1, 100])) == [0, 0, 0, 1, 1]
+assert list(checkio([0, 0, 0, -1, -1, 100])) == [0, 0, 0, -1, -1]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
