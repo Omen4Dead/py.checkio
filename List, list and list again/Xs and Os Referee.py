fe676b38b4448945a2c8f16514fdc583cbdb1168
Omen4Dead <69041761+Omen4Dead@users.noreply.github.com>
2024-05-07 22:13:55 +0500

    finally
    наконец победил задачу по крестикам-ноликам

--- List, list and list again/Xs and Os Referee.py
+++ List, list and list again/Xs and Os Referee.py
@@ -1,24 +1,31 @@
 def checkio(game_result: list[str]) -> str:
     # your code here
-    len_x = len(game_result[0])
-    len_y = len(game_result)
-    if "XXX" in game_result:
-        return "X"
-    if "OOO" in game_result:
-        return "O"
-    for y in range(len_y):
-        res = ''
-        diag_res1 = ''
-        for x in range(len_x):
-            print(('x: ', x, 'y: ', y), game_result[x][y])
-            res = res + game_result[x][y]
-            diag_res1 = diag_res1 + game_result[x][x]
-        print('res: ', res)
-        print('diag_res1: ', diag_res1)
-        if res == "XXX":
-            return "X"
-        if res == "OOO":
-            return "O"
+    rows = game_result
+    for i in rows:
+        if i == 'XXX': return 'X'
+        if i == 'OOO': return 'O'
+    len_x = len(rows[0])
+    cols = []
+    for idx in range(len_x):
+        col = ''
+        for idx2 in range(len_x):
+            col = col + rows[idx2][idx]
+        cols.append(col)
+        if col == 'XXX': return 'X'
+        if col == 'OOO': return 'O'
+    # print(cols, rows)
+    diag_lr = ''
+    for i in range(len_x):
+        diag_lr = diag_lr + rows[i][i]
+    if diag_lr == 'XXX': return 'X'
+    if diag_lr == 'OOO': return 'O'
+
+    diag_rl = ''
+    for i in range(len_x):
+        diag_rl = diag_rl + rows[i][::-1][i]
+    diag_lr = diag_lr + rows[i][i]
+    if diag_rl == 'XXX': return 'X'
+    if diag_rl == 'OOO': return 'O'
     return "D"
 
 
@@ -28,6 +35,7 @@ print("Example:")
 assert checkio(["X.O", "XX.", "XOO"]) == "X"
 assert checkio(["OO.", "XOX", "XOX"]) == "O"
 assert checkio(["OOX", "XXO", "OXX"]) == "D"
+assert checkio(['XOO', '.X.', 'X.X']) == 'X'
 assert checkio(["O.X", "XX.", "XOO"]) == "X"
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
